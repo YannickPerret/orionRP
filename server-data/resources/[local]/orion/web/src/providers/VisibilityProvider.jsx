c6f14995f3bf386694bba4ccf86a99fe54a3bab1
Yannick Perret <contact@yannickperret.com>
2023-11-11 14:34:26 +0100

    added reactjs frontend

---
+++ server-data/resources/[local]/orion/web/src/providers/VisibilityProvider.jsx
@@ -0,0 +1,52 @@
+import React, {
+  Context,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { useNuiEvent } from "../hooks/useNuiEvent";
+import { fetchNui } from "../utils/fetchNui";
+import { isEnvBrowser } from "../utils/misc";
+
+const VisibilityCtx = createContext(null);
+
+export const VisibilityProvider = ({ children }) => {
+  const [visible, setVisible] = useState(false);
+
+  useNuiEvent("setVisible", setVisible);
+
+  // Handle pressing escape/backspace
+  useEffect(() => {
+    // Only attach listener when we are visible
+    if (!visible) return;
+
+    const keyHandler = (e) => {
+      if (["Backspace", "Escape"].includes(e.code)) {
+        if (!isEnvBrowser()) fetchNui("hideFrame");
+        else setVisible(!visible);
+      }
+    };
+
+    window.addEventListener("keydown", keyHandler);
+
+    return () => window.removeEventListener("keydown", keyHandler);
+  }, [visible]);
+
+  return (
+    <VisibilityCtx.Provider
+      value={{
+        visible,
+        setVisible,
+      }}
+    >
+      <div
+        style={{ visibility: visible ? "visible" : "hidden", height: "100%" }}
+      >
+        {children}
+      </div>
+    </VisibilityCtx.Provider>
+  );
+};
+
+export const useVisibility = () => useContext(VisibilityCtx);
