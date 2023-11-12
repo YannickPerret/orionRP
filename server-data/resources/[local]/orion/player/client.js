e40ef87a5d6b100989d7e8458fd0000f7581686b
Yannick Perret <contact@yannickperret.com>
2023-11-12 19:58:49 +0100

    test

--- server-data/resources/[local]/orion/player/client.js
+++ server-data/resources/[local]/orion/player/client.js
@@ -53,29 +53,36 @@ RegisterCommand(
     const blip = GetFirstBlipInfoId(8); // ID 8 correspond à un waypoint
     if (blip !== 0) {
       const coord = GetBlipInfoIdCoord(blip);
-
-      // Demander la collision à l'emplacement pour s'assurer que le sol est chargé
       RequestCollisionAtCoord(coord[0], coord[1], coord[2]);
 
-      // Trouver la hauteur du sol à cet emplacement
-      const [, groundZ] = GetGroundZFor_3dCoord(
-        coord[0],
-        coord[1],
-        coord[2],
-        0,
-        false
-      );
+      // Attendre que la hauteur du sol soit chargée
+      let groundZ = 0;
+      let groundCheck = false;
 
-      // Téléporter le joueur à l'emplacement avec la hauteur du sol correcte
-      SetEntityCoordsNoOffset(
-        playerPed,
-        coord[0],
-        coord[1],
-        groundZ + 1.0,
-        false,
-        false,
-        true
-      ); // Ajouté un petit offset pour éviter de se retrouver sous le sol
+      setTimeout(() => {
+        [groundCheck, groundZ] = GetGroundZFor_3dCoord(
+          coord[0],
+          coord[1],
+          coord[2],
+          0,
+          false
+        );
+        if (groundCheck) {
+          SetEntityCoordsNoOffset(
+            playerPed,
+            coord[0],
+            coord[1],
+            groundZ + 1.0,
+            false,
+            false,
+            true
+          ); // Ajouté un petit offset pour éviter de se retrouver sous le sol
+        } else {
+          console.log(
+            "Impossible de trouver la hauteur du sol. Téléportation annulée."
+          );
+        }
+      }, 1000); // Attendre 1 seconde pour laisser le temps au jeu de charger la hauteur du sol
     } else {
       console.log("Aucun waypoint trouvé.");
     }
