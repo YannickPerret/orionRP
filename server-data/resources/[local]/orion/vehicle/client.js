a0c3c8854ac0ddb0d9479e98b25bf5f7ea7560be
Yannick Perret <contact@yannickperret.com>
2023-11-13 22:51:29 +0100

    fix ui speed

--- server-data/resources/[local]/orion/vehicle/client.js
+++ server-data/resources/[local]/orion/vehicle/client.js
@@ -107,7 +107,7 @@ const playSound = (sound) => {
           vehAcc > seatbeltEjectAccel * 9.81
         );
         if (
-          GetEntitySpeedVector(veh, true).y > 1.0 &&
+          vehIsMovingFwd &&
           prevSpeed > seatbeltEjectSpeed / 2.237 &&
           vehAcc > seatbeltEjectAccel * 9.81
         ) {
