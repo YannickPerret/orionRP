a6c4248c053f4463b4d1f56b052ecb852f994fc4
Yannick Perret <contact@yannickperret.com>
2023-11-13 23:52:09 +0100

    disable respawn

--- server-data/resources/[gamemodes]/basic-gamemode/basic_client.lua
+++ server-data/resources/[gamemodes]/basic-gamemode/basic_client.lua
@@ -1,4 +1,6 @@
 AddEventHandler('onClientMapStart', function()
-  exports.spawnmanager:setAutoSpawn(true)
-  exports.spawnmanager:forceRespawn()
+  exports.spawnmanager:setAutoSpawn(false)
+  exports.spawnmanager:spawnPlayer()
+  SetClockTime(24, 0, 0)
+    PauseClock(true)
 end)
