22d076b8f231becb34ba2af0856af5f02aa9ea5b
Yannick Perret <contact@yannickperret.com>
2023-11-26 16:09:20 +0100

    added give monney

--- server-data/resources/[local]/orion/utils/player.js
+++ server-data/resources/[local]/orion/utils/player.js
@@ -107,8 +107,8 @@ exports('findNearbyPlayers', maxDistance => {
     if (NetworkIsPlayerActive(serverId)) {
       const targetPlayerId = GetPlayerServerId(serverId);
 
-      console.log('targetPlayerId', targetPlayerId, 'mainPlayerId', playerPed);
-      if (targetPlayerId !== mainPlayerServerId) {
+      console.log('targetPlayerId', targetPlayerId, 'mainPlayerId', GetPlayerServerId(PlayerId()));
+      if (targetPlayerId !== GetPlayerServerId(PlayerId())) {
         const targetPed = GetPlayerPed(targetPlayerId);
         const targetPlayerCoords = GetEntityCoords(targetPed, true); // Get the coordinates of the target player
         const distance = GetDistanceBetweenCoords(
