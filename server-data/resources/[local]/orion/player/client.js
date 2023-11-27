2999dc96c62f03cedaa59eb3ae91dd5080921457
Yannick Perret <contact@yannickperret.com>
2023-11-27 23:25:22 +0100

    continue custom skin creator

--- server-data/resources/[local]/orion/player/client.js
+++ server-data/resources/[local]/orion/player/client.js
@@ -176,7 +176,7 @@ on('__cfx_nui:updateSkin', async (data, cb) => {
 
   ApplyPlayerBodySkin(PlayerId(), {
     Model: {
-      Hash: model,
+      Hash: GetHashKey(model),
       Father: data.dad,
       Mother: data.mom,
       ShapeMix: data.heritage * 0.1,
@@ -197,6 +197,7 @@ on('__cfx_nui:updateSkin', async (data, cb) => {
       //ChestHairColor: data.chestHairColor,
     },
   });
+
   cb({ ok: true });
 });
 
@@ -370,3 +371,73 @@ const ApplyPlayerBodySkin = (playerId, bodySkin) => {
   //ApplyPedTattoos(ped, bodySkin.Tattoos || {})
   //ApplyPedProps(ped, bodySkin);
 };
+
+/*
+function GetDefaultBodySkin(gender) {
+    let modelHash = GetHashKey("mp_m_freemode_01")
+
+    if (gender == 1) {
+        modelHash = GetHashKey("mp_f_freemode_01")
+    }
+
+    return {
+        Model = {Hash = modelHash, Father = 0, Mother = 23, ShapeMix = 0.5, SkinMix = 0.5},
+        Hair = {
+            HairType = 0,
+            HairColor = 0,
+            HairSecondaryColor = 0,
+            EyebrowType = -1,
+            EyebrowOpacity = 1.0,
+            EyebrowColor = 0,
+            BeardType = -1,
+            BeardOpacity = 1.0,
+            BeardColor = 0,
+            ChestHairType = -1,
+            ChestHairOpacity = 1.0,
+            ChestHairColor = 0,
+        },
+        FaceTrait = {
+            EyeColor = -1,
+            Blemish = -1,
+            Ageing = -1,
+            Complexion = -1,
+            Moles = -1,
+            BodyBlemish = -1,
+            AddBodyBlemish = -1,
+            CheeksBoneHigh = 0.0,
+            CheeksBoneWidth = 0.0,
+            CheeksWidth = 0.0,
+            ChimpBoneLength = 0.0,
+            ChimpBoneLower = 0.0,
+            ChimpBoneWidth = 0.0,
+            ChimpHole = 0.0,
+            EyebrowForward = 0.0,
+            EyebrowHigh = 0.0,
+            EyesOpening = 0.0,
+            JawBoneBackLength = 0.0,
+            JawBoneWidth = 0.0,
+            LipsThickness = 0.0,
+            NeckThickness = 0.0,
+            NoseBoneHigh = 0.0,
+            NoseBoneTwist = 0.0,
+            NosePeakLength = 0.0,
+            NosePeakLower = 0.0,
+            NosePeakHeight = 0.0,
+            NoseWidth = 0.0,
+        },
+        Makeup : {
+            LipstickType = -1,
+            LipstickOpacity = 1.0,
+            LipstickColor = 0,
+            BlushType = -1,
+            BlushOpacity = 1.0,
+            BlushColor = 0,
+            FullMakeupType = -1,
+            FullMakeupOpacity = 1.0,
+            FullMakeupDefaultColor = true,
+            FullMakeupPrimaryColor = 0,
+            FullMakeupSecondaryColor = 0,
+        },
+        Tattoos : {},
+    }
+  }*/
