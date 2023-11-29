568e2a3854f9cf2eee95899aa870d60c47d177b6
Yannick Perret <contact@yannickperret.com>
2023-11-29 22:59:50 +0100

    fix import

--- server-data/resources/[local]/orion/station/client.js
+++ server-data/resources/[local]/orion/station/client.js
@@ -1,8 +1,8 @@
 const gazStations = require('./gasStations.js');
 
 async () => {
-  for (let i = 0; i < gasStations.length; i++) {
-    const station = gasStations[i];
+  for (let i = 0; i < gazStations.length; i++) {
+    const station = gazStations[i];
     createBlip(station.coordinates, 361, 1, 1, 'Station essence');
   }
 };
