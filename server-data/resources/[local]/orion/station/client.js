013d52f39a391d5ec26ef0faff0e822742e0f67f
Yannick Perret <contact@yannickperret.com>
2023-11-29 23:23:14 +0100

    fix import

--- server-data/resources/[local]/orion/station/client.js
+++ server-data/resources/[local]/orion/station/client.js
@@ -890,6 +890,6 @@ const gazStationsBlips = [
 (async () => {
   for (let i = 0; i < gazStationsBlips.length; i++) {
     const station = gazStationsBlips[i];
-    createBlip(station.coordinates, 361, 1, 1, 'Station essence');
+    createBlip(station.coordinates, 361, 0, 'Station essence');
   }
 })();
