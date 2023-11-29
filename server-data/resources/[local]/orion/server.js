e940b257b039c568ab1c0813aaed7f2c3b99dfc8
Yannick Perret <contact@yannickperret.com>
2023-11-29 18:11:33 +0100

    fix import

--- server-data/resources/[local]/orion/server.js
+++ server-data/resources/[local]/orion/server.js
@@ -1,5 +1,5 @@
 const PlayerManager = require('./system/playerManager.js');
-const { db, r } = require('../system/database.js');
+const { db, r } = require('./system/database.js');
 
 on('playerDropped', reason => {
   let sourceId = global.source; // Obtenez l'ID unique du joueur
