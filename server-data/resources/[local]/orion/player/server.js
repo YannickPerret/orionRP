ebe19cbbbd215244915a40ec2e252ef9762889f3
Yannick Perret <contact@yannickperret.com>
2023-11-29 20:08:10 +0100

    fix import

--- server-data/resources/[local]/orion/player/server.js
+++ server-data/resources/[local]/orion/player/server.js
@@ -1,4 +1,4 @@
-const Player = require('player.js');
+const Player = require('./player.js');
 // Position par défaut du joueur
 const playerPosition = [-530.77, -2113.83, 9.0];
 const enableDiscordWhitelist = true;
