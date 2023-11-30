96fce5d47c235bd081672574ac3b6ef5638a9b62
Yannick Perret <contact@yannickperret.com>
2023-11-30 19:39:28 +0100

    fix class naming import

--- server-data/resources/[local]/orion/web/src/window/SkinCreator/SkinCreator.jsx
+++ server-data/resources/[local]/orion/web/src/window/SkinCreator/SkinCreator.jsx
@@ -184,8 +184,8 @@ export default function SkinCreator() {
      </div>
 
       <div className='skinCreator__valid'>
-        <button onClick={() => handleReset}>Reset</button>
-        <button onClick={() => handleValidate}>Valider</button>
+        <button onClick={handleReset}>Reset</button>
+        <button onClick={handleValidate}>Valider</button>
       </div>
     </div>
   );
