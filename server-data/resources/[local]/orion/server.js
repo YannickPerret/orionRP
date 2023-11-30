ecf183c26247f3cf028cfc905c57e6fbd31dfc5e
Yannick Perret <contact@yannickperret.com>
2023-11-30 10:14:12 +0100

    vehicle fuel

--- server-data/resources/[local]/orion/server.js
+++ server-data/resources/[local]/orion/server.js
@@ -1,4 +1,3 @@
-const PlayerManager = require('./core/playerManager.js');
 const VehicleManager = require('./core/vehicleManager.js');
 const Player = require('./player/player.js');
 const Vehicle = require('./vehicle/vehicle.js');
