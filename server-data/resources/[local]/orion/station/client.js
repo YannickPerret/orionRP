38d50ffb4aaea9b72fd040ae5307ef3e0b5dab02
Yannick Perret <contact@yannickperret.com>
2023-11-30 11:15:03 +0100

    vehicle fuel

--- server-data/resources/[local]/orion/station/client.js
+++ server-data/resources/[local]/orion/station/client.js
@@ -1,10 +1,8 @@
 (async () => {
   try {
-    const gazStationsString = LoadResourceFile(GetCurrentResourceName(), './gasStations.json');
-    console.log(gazStationsString);
+    const gazStationsString = LoadResourceFile(GetCurrentResourceName(), 'gasStations.json');
     const gazStationsBlips = JSON.parse(gazStationsString);
 
-    console.log(gazStationsBlips);
     for (let i = 0; i < gazStationsBlips.GasStations.length; i++) {
       const station = gazStationsBlips.GasStations[i];
       createBlip(station.coordinates, 361, 0, 'Station essence');
