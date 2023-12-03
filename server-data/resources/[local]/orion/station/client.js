fe24b844b4efa15ecafa054068006043ede4d41c
Yannick Perret <contact@yannickperret.com>
2023-12-03 16:34:27 +0100

    rework customization

--- server-data/resources/[local]/orion/station/client.js
+++ server-data/resources/[local]/orion/station/client.js
@@ -29,6 +29,7 @@ const gazStationsBlips = JSON.parse(gazStationsString);
             true
           );
 
+          console.log(distance);
           if (distance < 2) {
             if (!IsPedInAnyVehicle(PlayerPedId(), false)) {
               //DrawText3Ds(pump.x, pump.y, pump.z, 'Appuyez sur ~g~E~w~ pour prendre un tuyau');
