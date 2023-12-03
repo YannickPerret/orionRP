5859ecb2fdc60e534f3ae5610558805224dfc748
Yannick Perret <contact@yannickperret.com>
2023-12-03 14:33:05 +0100

    rework customization

--- server-data/resources/[local]/orion/web/src/components/vehicle/fuel.jsx
+++ server-data/resources/[local]/orion/web/src/components/vehicle/fuel.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './fuel.css'
 
 export default function Fuel({fuel}) {
+  console.log(fuel)
   return (
     <div className="progress">
         <div className="progress-bar" role="progressbar" style={{width: fuel + '%'}} aria-valuenow={fuel} aria-valuemin="0" aria-valuemax="100"></div>
