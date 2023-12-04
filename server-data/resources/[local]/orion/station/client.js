bcab4d3946daa8427ecca54155b0fc2fbb1cfdc2
Yannick Perret <contact@yannickperret.com>
2023-12-04 19:35:01 +0100

    fix fuel

--- server-data/resources/[local]/orion/station/client.js
+++ server-data/resources/[local]/orion/station/client.js
@@ -81,7 +81,6 @@ const getClosestPumpHandle = () => {
   let distance = 10.0;
 
   for (let model of pumpModels) {
-    console.log(model);
     const handle = GetClosestObjectOfType(pedCoords.x, pedCoords.y, pedCoords.z, 2.0, model, false, false, false);
     if (handle !== 0) {
       let objcoords = GetEntityCoords(handle);
