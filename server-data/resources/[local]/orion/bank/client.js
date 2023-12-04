4b732456c98c46854d0ff16d5c6f4f4d8a60fca5
Yannick Perret <contact@yannickperret.com>
2023-12-04 23:45:57 +0100

    create bank

--- server-data/resources/[local]/orion/bank/client.js
+++ server-data/resources/[local]/orion/bank/client.js
@@ -2,7 +2,8 @@ const bankCoordsJson = JSON.parse(LoadResourceFile(GetCurrentResourceName(), 'ba
 
 (async () => {
   const bankBlips = [];
-  for (const bankCoords of bankCoordsJson) {
+  for (const bankCoords of bankCoordsJson.bank) {
     createBlip(bankCoords.coords, 108, 0, 'Banque');
+    bankBlips.push(bankCoords.coords);
   }
 })();
