9e9759b7114271096b41f46f8b276617ddbc4dbc
Yannick Perret <contact@yannickperret.com>
2023-12-04 18:03:53 +0100

    fix fuel

--- server-data/resources/[local]/orion/station/client.js
+++ server-data/resources/[local]/orion/station/client.js
@@ -107,6 +107,7 @@ const grabPipeFromPump = async (ped, pump) => {
   //pipeLocation = GetEntityCoords(pipeProps);
 
   pipeLocation = GetOffsetFromEntityInWorldCoords(pipeProps, 0.0, -0.033, -0.195);
+  console.log('pipeLocation', pipeLocation);
   pumpModels.map(hash => {
     pumpHandle = GetClosestObjectOfType(GetEntityCoords(ped, false), 1.5, hash, true, true, true);
   });
