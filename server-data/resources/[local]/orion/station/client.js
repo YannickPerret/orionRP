932157f70365eca53c1d5506c33b44f0195f2a90
Yannick Perret <contact@yannickperret.com>
2023-12-04 20:21:38 +0100

    fix fuel

--- server-data/resources/[local]/orion/station/client.js
+++ server-data/resources/[local]/orion/station/client.js
@@ -171,7 +171,7 @@ const createNozzle = async () => {
 
 const grabRope = () => {
   let prop = 'w_at_scope_small';
-  let anchorPos = GetOffsetFromEntityInWorldCoords(pump, 0.0, 0.0, 0.0);
+  let anchorPos = GetEntityCoords(pump);
   ropeAnchor = CreateObject(GetHashKey(prop), anchorPos.x, anchorPos.y, anchorPos.z + 3.2, true, true, true);
 
   return anchorPos;
