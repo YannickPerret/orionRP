bcf8b9a992c65b7cb3a4adbd6119efd11b194a20
Yannick Perret <contact@yannickperret.com>
2023-12-07 19:53:08 +0100

    rework core

--- server-data/resources/[local]/orion/core/server/server.js
+++ server-data/resources/[local]/orion/core/server/server.js
@@ -1,4 +1,4 @@
-const PlayerManagerServer = require('./core/playerManager.js');
+const PlayerManagerServer = require('./core/server/playerManager.js');
 
 on('playerDropped', reason => {
   let sourceId = global.source; // Obtenez l'ID unique du joueur
