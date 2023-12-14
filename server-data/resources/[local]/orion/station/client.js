d21b956bbfdac090aef1b043eb7b2fab1eb66e64
Yannick Perret <contact@yannickperret.com>
2023-12-14 12:51:20 +0100

    station

--- server-data/resources/[local]/orion/station/client.js
+++ server-data/resources/[local]/orion/station/client.js
@@ -395,8 +395,7 @@
     let [pumpPropCoordsX, pumpPropCoordsY, pumpPropCoordsZ] = GetOffsetFromEntityInWorldCoords(IdProp, 0.0, -0.019, -0.1749);
     currentRope[playerId] = AddRope(posPump[0], posPump[1], posPump[2] + 1.76, 0.0, 0.0, 0.0, 5.0, 1, 1000.0, 0.5, 1.0, false, false, false, 5.0, false, 0);
 
-    AttachEntitiesToRope(currentRope[playerId][0], IdProp, currentPumpObj[playerId], pumpPropCoordsX, pumpPropCoordsY, pumpPropCoordsZ, posPump[0], posPump[1], posPump[2], 30.0, 0, 0);
-
+    AttachEntitiesToRope(currentRope[playerId][0], IdProp, currentPumpObj[playerId], pumpPropCoordsX, pumpPropCoordsY, pumpPropCoordsZ, posPump[0], posPump[1], posPump[2]+1, 30.0, 0, 0);
   })
 
   onNet('orion:station:c:DetachRope', (playerId) => {
