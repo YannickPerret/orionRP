cab81b075ac009d2b1003eac885304060196f5bd
Yannick Perret <contact@yannickperret.com>
2023-12-14 11:50:10 +0100

    station

--- server-data/resources/[local]/orion/station/server.js
+++ server-data/resources/[local]/orion/station/server.js
@@ -1,13 +1,16 @@
-onNet('orion:station:s:AttachRope', (netIdProp, coordPumps, model) => {
-    const source = global.source;
-    const player = PlayerManager.getPlayerBySource(source);
+(async () => {
+    onNet('orion:station:s:AttachRope', (netIdProp, coordPumps, model) => {
+        const source = global.source;
+        const player = PlayerManager.getPlayerBySource(source);
+    
+        emitNet('orion:station:c:AttachRope', source, netIdProp, coordPumps, model, player.id);
+    })
+    
+    onNet('orion:station:s:DetachRope', (netIdProp) => {
+        const source = global.source;
+        const player = PlayerManager.getPlayerBySource(source);
+    
+        emitNet('orion:station:c:DetachRope', source, netIdProp, player.id);
+    })
 
-    emitNet('orion:station:c:AttachRope', source, netIdProp, coordPumps, model, player.id);
-})
-
-onNet('orion:station:s:DetachRope', (netIdProp) => {
-    const source = global.source;
-    const player = PlayerManager.getPlayerBySource(source);
-
-    emitNet('orion:station:c:DetachRope', source, netIdProp, player.id);
 })
