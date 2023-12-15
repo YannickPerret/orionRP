736c3cbd9153e3d96fe515561e3a3c1e496f894e
Yannick Perret <contact@yannickperret.com>
2023-12-15 20:47:23 +0100

    dd

--- server-data/resources/[local]/orion/voice/client.js
+++ server-data/resources/[local]/orion/voice/client.js
@@ -1,6 +1,16 @@
 (async () => {
     const OpusScript = require("opusscript");
+    const socket = "http://127.0.0.1:28469"
+    let localStream;
+    let peerConnection;
+    const configuration = {'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}]}; // Configuration STUN/TURN
 
+    const sampleRate = 48000; // Taux d'échantillonnage typique pour Opus
+    const channels = 2; // Stéréo
+    const encoder = new OpusScript(sampleRate, channels, OpusScript.Application.AUDIO);
+
+
+    
     SetTick(() => {
         // Commencez à capturer l'audio ici
         // Éventuellement, encodez l'audio avec Opus
@@ -13,6 +23,107 @@
 
     RegisterKeyMapping('Talk', 'Talk', 'keyboard', 'N');
     RegisterCommand('Talk', () => {
+        let inputMicrophone = GetProfileSetting(724);
+        if (inputMicrophone == 0) {
+            emit('orion:showNotification', 'Vous devez avoir un microphone pour parler.');
+            return;
+        }
+
         emitNet('orion:voice:s:toggle', GetPlayerServerId(PlayerId()));
     }, false);
+
+
+
+
+    // Commencer la capture audio
+    function startCapture() {
+        navigator.mediaDevices.getUserMedia({ audio: true, video: false, echoCancellation: true, noiseSuppression: true })
+        .then(stream => {
+            const audioContext = new AudioContext();
+            const source = audioContext.createMediaStreamSource(stream);
+            const processor = audioContext.createScriptProcessor(4096, 1, 1);
+
+            source.connect(processor);
+            processor.connect(audioContext.destination);
+
+            processor.onaudioprocess = e => {
+                const inputData = e.inputBuffer.getChannelData(0); // ou stéréo si nécessaire
+                const outputData = new Float32Array(inputData.length);
+
+                // Encodez ici avec OpusScript
+                const encodedData = encoder.encode(inputData);
+                
+                // Envoyez les données encodées
+                // Par exemple, via WebSocket ou une connexion WebRTC
+            };
+    }).catch(err => console.error(err));
+    }
+
+    // Créer la connexion peer
+    function createPeerConnection() {
+        try {
+            peerConnection = new RTCPeerConnection(configuration);
+
+            // Envoyer les ICE candidates au serveur
+            peerConnection.onicecandidate = event => {
+                if (event.candidate) {
+                    socket.emit('relayICECandidate', {
+                        'candidate': event.candidate
+                    });
+                }
+            };
+        } catch (err) {
+            console.error('Erreur lors de la création de la connexion peer : ', err);
+        }
+    }
+
+    // Gestionnaire pour envoyer l'audio
+    function handleKeyDown(event) {
+        if (event.key === 'n' || event.key === 'N') {
+            if (!localStream) {
+                console.log("Micro non capturé ou non autorisé");
+                return;
+            }
+
+            if (!peerConnection) createPeerConnection();
+
+            localStream.getTracks().forEach(track => {
+                peerConnection.addTrack(track, localStream);
+            });
+
+            // Créer une offre et l'envoyer via le serveur
+            peerConnection.createOffer()
+                .then(offer => peerConnection.setLocalDescription(offer))
+                .then(() => {
+                    socket.emit('relaySessionDescription', {
+                        'sessionDescription': peerConnection.localDescription
+                    });
+                }).catch(err => console.error('Erreur lors de la création de l\'offre : ', err));
+
+            console.log('Envoi du flux audio');
+        }
+    }
+
+    // Gestionnaire pour arrêter l'envoi de l'audio
+    function handleKeyUp(event) {
+        if (event.key === 'n' || event.key === 'N') {
+            // Ici, vous pouvez ajouter la logique pour arrêter d'envoyer le flux audio
+            console.log('Arrêt de l\'envoi du flux audio');
+        }
+    }
+
+    // Ecouteurs d'événements
+    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener('keyup', handleKeyUp);
+
+    // Socket.io handlers pour la signalisation
+    socket.on('iceCandidate', (config) => {
+        peerConnection.addIceCandidate(new RTCIceCandidate(config.candidate));
+    });
+
+    socket.on('sessionDescription', (config) => {
+        peerConnection.setRemoteDescription(new RTCSessionDescription(config.sessionDescription));
+    });
+
+    startCapture();
 })();
--- voice/index.js
+++ voice/index.js
@@ -1,61 +1,85 @@
 const express = require('express');
-const app = express();
 const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
 const server = http.createServer(app);
-const OpusScript = require("opusscript");
-const sodium = require("libsodium")
-const { Server } = require("socket.io");
-const io = new Server(server);
+const io = socketIo(server);
 
-const lobbyDefault = new Set();
-const lobbyGame = new Set();
+app.use(express.static('public'));
 
+let channels = {};
+let sockets = {};
 
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
-});
-
-io.on('connection', (socket) => {
-    lobbyDefault.add(socket.id);
-  
-    socket.on('joinGame', () => {
-      lobbyDefault.delete(socket.id);
-      lobbyGame.add(socket.id);
-
-      // Gérez ici la logique de connexion au jeu
-    });
-  
-    socket.on('leaveGame', () => {
-      lobbyGame.delete(socket.id);
-      lobbyDefault.add(socket.id);
-
-      // Gérez ici la logique de déconnexion du jeu
-    });
-  
-    // Créez un encodeur Opus
-    const encoder = new OpusScript(48000, 2);
-
-    socket.on('audioMessage', (audioData) => {
-        // Encodez l'audio avec Opus
-        const encoded = encoder.encode(audioData);
-
-        lobbyGame.forEach((clientId) => {
-        if (clientId !== socket.id) {
-            io.to(clientId).emit('receiveAudio', encoded);
-        }
-        });
-    });
-
+io.on('connection', socket => {
+    socket.channels = {};
+    sockets[socket.id] = socket;
+    console.log(`[${socket.id}] connection accepted`);
 
     socket.on('disconnect', () => {
-      lobbyDefault.delete(socket.id);
-      lobbyGame.delete(socket.id);
+        for (let channel in socket.channels) {
+            part(channel);
+        }
+        console.log(`[${socket.id}] disconnected`);
+        delete sockets[socket.id];
     });
-  });
 
+    socket.on('join-room', config => {
+        let channel = config.channel;
+        if (channel in socket.channels) {
+            console.log(`[${socket.id}] ERROR: already joined `, channel);
+            return;
+        }
+        if (!(channel in channels)) {
+            channels[channel] = {};
+        }
+        for (let id in channels[channel]) {
+            channels[channel][id].emit('addPeer-room', {'peer_id': socket.id, 'should_create_offer': false});
+            socket.emit('addPeer-room', {'peer_id': id, 'should_create_offer': true});
+        }
+        socket.join(channel);
+        socket.broadcast.to(channel).emit('room-users', config);
+        channels[channel][socket.id] = socket;
+        socket.channels[channel] = channel;
+    });
 
-server.listen(28469, () => {
-  console.log('listening on *:28469');
+    const part = channel => {
+        if (!(channel in socket.channels)) {
+            console.log(`[${socket.id}] ERROR: not in `, channel);
+            return;
+        }
+        delete socket.channels[channel];
+        delete channels[channel][socket.id];
+        for (let id in channels[channel]) {
+            channels[channel][id].emit('removePeer', {'peer_id': socket.id});
+            socket.emit('removePeer', {'peer_id': id});
+        }
+    };
+
+    socket.on('part', part);
+
+    socket.on('relayICECandidate-room', config => {
+        let peer_id = config.peer_id;
+        let ice_candidate = config.ice_candidate;
+        console.log(`[${socket.id}] relaying ICE candidate to [${peer_id}] `, ice_candidate);
+        if (peer_id in sockets) {
+            sockets[peer_id].emit('iceCandidate-room', {'peer_id': socket.id, 'ice_candidate': ice_candidate});
+        }
+    });
+
+    socket.on('relaySessionDescription-room', config => {
+        let peer_id = config.peer_id;
+        let session_description = config.session_description;
+        console.log(`[${socket.id}] relaying session description to [${peer_id}] `, session_description);
+        if (peer_id in sockets) {
+            sockets[peer_id].emit('sessionDescription-room', {
+                'peer_id': socket.id,
+                'session_description': session_description
+            });
+        }
+    });
 });
 
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
