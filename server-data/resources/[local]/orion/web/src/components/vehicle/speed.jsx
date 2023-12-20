2003a4a65d98ad649eee8117648fa6fea5b14149
Yannick Perret <contact@yannickperret.com>
2023-12-20 23:36:35 +0100

    update ui

--- server-data/resources/[local]/orion/web/src/components/vehicle/speed.jsx
+++ server-data/resources/[local]/orion/web/src/components/vehicle/speed.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import { useData } from '../../utils/dataContext';
 
 export default function Speed() {
-  const [data, setData] = useData();
+  const { data, setData } = useData();
+
   return (
     <div>{data.vehicle.speed} km/h</div>
   )
