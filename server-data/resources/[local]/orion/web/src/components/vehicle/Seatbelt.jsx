09e58aec2974d83f573dbb348248a09a376fdf67
Yannick Perret <contact@yannickperret.com>
2023-12-20 23:29:54 +0100

    update ui

--- server-data/resources/[local]/orion/web/src/components/vehicle/Seatbelt.jsx
+++ server-data/resources/[local]/orion/web/src/components/vehicle/Seatbelt.jsx
@@ -1,13 +1,8 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Sealtbelt from '../../assets/images/seatbelt.svg'
 import './seatbelt.css'
 
 export default function Seatbelt() {
-
-  useEffect(() => {
-    console.log('Seatbelt')
-  }, [])
-
   return (
     <div className='seatbelt'>
       <img src={Sealtbelt} alt="Seatbelt" />
