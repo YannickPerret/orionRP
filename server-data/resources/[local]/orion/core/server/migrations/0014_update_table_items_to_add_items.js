c3939c98ab3b1e1e504d31a82725c69cf7762bde
Yannick Perret <contact@yannickperret.com>
2023-12-21 18:53:52 +0100

    update item database

--- server-data/resources/[local]/orion/core/server/migrations/0014_update_table_items_to_add_items.js
+++ server-data/resources/[local]/orion/core/server/migrations/0014_update_table_items_to_add_items.js
@@ -1,8 +1,9 @@
-const { itemsList } = require('../../shared/items.js');
+const itemsList = require('../../shared/items.js');
 
 module.exports = {
     version: 14,
     migrate: async (db) => {
+
         itemsList.forEach(async (item) => {
             console.log(`Adding item ${item.name}`)
             await db.insert('items', item);
