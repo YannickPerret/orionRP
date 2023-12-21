54bea089f5a1e464710e2cb68e82dc2ef55d1d05
Yannick Perret <contact@yannickperret.com>
2023-12-21 17:35:06 +0100

    update database and inventory system

--- server-data/resources/[local]/orion/core/server/migrations/0000_create_table_system.js
+++ server-data/resources/[local]/orion/core/server/migrations/0000_create_table_system.js
@@ -2,9 +2,10 @@
 module.exports = {
     version: 0,
     migrate: async (db) => {
+        await db.createTable('system')
         // Insertion d'un document avec un ID spécifique dans la table 'system'
         await db.insert('system', {
-            id: 1, // Définir l'ID du document à 1
+            id: '1',
             version: 0, // Version initiale
             lastUpdate: new Date(), // Date de la dernière mise à jour
             name: "Orion", // Autres champs
