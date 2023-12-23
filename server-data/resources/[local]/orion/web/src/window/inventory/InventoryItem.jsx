14e0b4479282e354805084ff6df4d699442da510
Yannick Perret <contact@yannickperret.com>
2023-12-23 23:55:40 +0100

    added bank

--- server-data/resources/[local]/orion/web/src/window/inventory/InventoryItem.jsx
+++ server-data/resources/[local]/orion/web/src/window/inventory/InventoryItem.jsx
@@ -10,7 +10,7 @@ export default function InventoryItem({ item }) {
     return (
         <div className={style.inventory__item}>
             <div className={style.inventory__item__icon}>
-                <img src={`./images/${item.image}`} alt="item icon" />
+                <img src={`./images/items/${item.image}`} alt="item icon" />
             </div>
             <div className={style.inventory__item__info}>
                 <h3>{item.quantity} {item.label}</h3>
