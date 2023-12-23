69d086f4ff1e50c0b1d49c77b25aeb7a3f5cb8cb
Yannick Perret <contact@yannickperret.com>
2023-12-23 22:26:03 +0100

    added bank

--- server-data/resources/[local]/orion/core/server/database.js
+++ server-data/resources/[local]/orion/core/server/database.js
@@ -239,7 +239,7 @@ class Database {
   }*/
 
   getByWithFilter(table, filters) {
-    console.log("ksflksf", table, filters)
+    console.log("ksflksf")
     return this.connect().then(connection => {
       let query = r.table(table);
 
