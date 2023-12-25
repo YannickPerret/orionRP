bfa9a64a795a345525c5b5583a0064a1401baa81
Yannick Perret <contact@yannickperret.com>
2023-12-25 15:39:52 +0100

    added bank

--- server-data/resources/[local]/orion/web/src/window/inventory/InventoryItemUsable.jsx
+++ server-data/resources/[local]/orion/web/src/window/inventory/InventoryItemUsable.jsx
@@ -3,28 +3,10 @@ import style from './inventory.module.scss';
 
 export default function InventoryItemUsable({ item, useItem, giveItem, dropItem }) {
 
-    const handleUseItem = (id) => {
-        console.log('use item')
-        useItem(id);
-    }
-
-    const handleGiveItem = (id) => {
-        console.log('give item')
-        giveItem(id);
-    }
-
-    const handleDropItem = (id) => {
-        console.log('drop item')
-        dropItem(id);
-    }
-
-    const clickOnUse = () => handleUseItem(item.id);
-    const clickOnGive = () => handleGiveItem(item.id);
-    const clickOnDrop = () => handleDropItem(item.id);
     return (
 
         <div className={style.inventory__item}>
-            <div onClick={clickOnUse}>
+            <div onClick={useItem(item.id)}>
                 <div className={style.inventory__item__icon}>
                     <img src={`./images/items/${item.image}`} alt="item icon" />
                 </div>
@@ -33,13 +15,13 @@ export default function InventoryItemUsable({ item, useItem, giveItem, dropItem 
                 </div>
             </div>
             <div className={style.inventory__item__actions}>
-                {item.useable && <button onClick={clickOnGive}>Donner</button>}
-                <button onClick={clickOnDrop}>Drop</button>
+                {item.useable && <button onClick={giveItem(item.id)}>Donner</button>}
+                <button onClick={dropItem(item.id)}>Drop</button>
             </div>
 
             <div className={style.inventory__item__description}>
                 {item.description}
             </div>
-        </div >
+        </div>
     )
 }
