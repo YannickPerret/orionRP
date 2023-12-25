5386ec59ebe0426f3c8f12e112bb6bfc4513569d
Yannick Perret <contact@yannickperret.com>
2023-12-25 15:03:13 +0100

    added bank

--- server-data/resources/[local]/orion/web/src/window/inventory/InventoryItemUsable.jsx
+++ server-data/resources/[local]/orion/web/src/window/inventory/InventoryItemUsable.jsx
@@ -6,7 +6,7 @@ export default function InventoryItemUsable({ item, useItem, giveItem, dropItem 
     return (
 
         <div className={style.inventory__item}>
-            <div onClick={() => useItem(item.id)}>
+            <div onClick={() => { console.log(`Item clicked: ${item.id}`); useItem(item.id); }}>
                 <div className={style.inventory__item__icon}>
                     <img src={`./images/items/${item.image}`} alt="item icon" />
                 </div>
