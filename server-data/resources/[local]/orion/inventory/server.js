3fcd57c5401897128c4d45f81f4468380216d0b3
Yannick Perret <contact@yannickperret.com>
2023-12-26 16:00:40 +0100

    added bank

--- server-data/resources/[local]/orion/inventory/server.js
+++ server-data/resources/[local]/orion/inventory/server.js
@@ -13,7 +13,7 @@
 
         console.log(playerInventory, itemInstance)
         if (playerInventory.hasItem(itemInstance)) {
-            if (Number(playerInventory.quantity) > 0) {
+            if (Number(itemInstance.quantity) > 0) {
                 console.log("has quantity")
                 if (itemInstance.type !== 'special') {
                     emitNet(`orion:inventory:c:useItem:${itemInstance.type}`, source, itemInstance);
