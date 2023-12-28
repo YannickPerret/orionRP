130647f2536fa7d2f6a8e80ef6f6b2534266264a
Yannick Perret <contact@yannickperret.com>
2023-12-28 10:49:51 +0100

    added bank

--- server-data/resources/[local]/orion/core/server/database.js
+++ server-data/resources/[local]/orion/core/server/database.js
@@ -9,26 +9,21 @@ class Database {
     this.db = db || 'orion';
     this.connection = null;
 
-    this.connect()
+    this.initConnection();
   }
 
-  connect() {
-    return new Promise((resolve, reject) => {
-      if (!this.connection) {
-        r.connect({ host: this.host, port: this.port, db: this.db }, (err, conn) => {
-          if (err) {
-            reject(err);
-          } else {
-            conn.use(this.db);
-            this.connection = conn;
-            resolve(conn);
-          }
-        });
-      } else {
-        resolve(this.connection);
+  async initConnection() {
+    if (!this.connection) {
+      try {
+        this.connection = await r.connect({ host: this.host, port: this.port, db: this.db });
+        this.connection.use(this.db);
+      } catch (err) {
+        console.error('Erreur lors de la connexion à la base de données:', err);
+        throw err;
       }
-    });
+    }
   }
+
 
   async initializeMigration() {
     let latestVersion = -1;
@@ -40,15 +35,17 @@ class Database {
   }
 
   async getLatestDbVersion() {
-    return this.connect().then(connection => {
-      return r.table('system').get("1")
-        .run(connection)
-        .then(system => system ? system.version : 0)
-        .catch(err => {
-          console.error('Erreur lors de la récupération de la version de la base de données:', err);
-          throw err;
-        });
-    });
+    if (!this.connection) {
+      await this.initConnection();
+    }
+
+    return r.table('system').get("1")
+      .run(this.connection)
+      .then(system => system ? system.version : 0)
+      .catch(err => {
+        console.error('Erreur lors de la récupération de la version de la base de données:', err);
+        throw err;
+      });
   }
 
   async applyMigrations(currentVersion) {
@@ -74,226 +71,224 @@ class Database {
   }
 
 
-  createDatabase(dbName) {
-    return this.connect().then(connection => {
-      return r.dbList().contains(dbName).run(connection)
-        .then(exists => {
-          if (!exists) {
-            return r.dbCreate(dbName).run(connection)
-              .then(result => {
-                console.log('Base de données créée avec succès');
-                return result;
-              });
-          } else {
-            console.log('La base de données existe déjà');
-            return Promise.resolve('La base de données existe déjà');
-          }
-        })
-        .catch(err => {
-          console.error('Erreur lors de la vérification ou de la création de la base de données:', err);
-          throw err;
-        });
-    });
+  async createDatabase(dbName) {
+    if (!this.connection) {
+      await this.initConnection();
+    }
+
+    try {
+      const exists = await r.dbList().contains(dbName).run(this.connection);
+      if (!exists) {
+        const result = await r.dbCreate(dbName).run(this.connection);
+        console.log('Base de données créée avec succès');
+        return result;
+      } else {
+        console.log('La base de données existe déjà');
+        return 'La base de données existe déjà';
+      }
+    } catch (err) {
+      console.error('Erreur lors de la vérification ou de la création de la base de données:', err);
+      throw err;
+    }
   }
 
-  updateVersion(version) {
-    return this.connect().then(connection => {
-      return r.table('system')
-        .get("1")
-        .update({
-          version: version,
-          lastUpdate: new Date()
-        })
-        .run(connection)
-        .then(result => {
-          console.log('Version de la base de données mise à jour avec succès');
-          return result;
-        })
-        .catch(err => {
-          console.error("Erreur lors de la mise à jour de la version de la base de données:", err);
-          throw err;
-        });
-    });
+  async updateVersion(version) {
+    if (!this.connection) {
+      await this.initConnection();
+    }
+
+    return r.table('system')
+      .get("1")
+      .update({ version, lastUpdate: new Date() })
+      .run(this.connection)
+      .then(result => {
+        console.log('Version de la base de données mise à jour avec succès');
+        return result;
+      })
+      .catch(err => {
+        console.error("Erreur lors de la mise à jour de la version de la base de données:", err);
+        throw err;
+      });
   }
 
 
 
 
-  createTable(tableName) {
-    return this.connect().then(connection => {
-      return r
-        .tableList()
-        .run(connection)
-        .then(list => {
-          if (!list.includes(tableName)) {
-            return r
-              .tableCreate(tableName)
-              .run(connection)
-              .then(result => {
-                console.log('Table créée avec succès');
-                return result;
-              })
-              .catch(err => {
-                console.error('Erreur lors de la création de la table:', err);
-                throw err;
-              });
-          } else {
-            console.log('La table existe déjà');
-            throw "La table existe déjà"
-          }
-        })
-        .catch(err => {
-          console.error('Erreur lors de la récupération de la liste des tables:', err);
-          throw err;
-        });
-    });
+  async createTable(tableName) {
+    if (!this.connection) {
+      await this.initConnection();
+    }
+
+    try {
+      const list = await r.tableList().run(this.connection);
+      if (!list.includes(tableName)) {
+        const result = await r.tableCreate(tableName).run(this.connection);
+        console.log('Table créée avec succès');
+        return result;
+      } else {
+        console.log('La table existe déjà');
+        return 'La table existe déjà';
+      }
+    } catch (err) {
+      console.error('Erreur lors de la création de la table:', err);
+      throw err;
+    }
   }
 
-  insert(table, data) {
-    return this.connect().then(connection => {
-      return r
-        .table(table)
-        .insert(data)
-        .run(connection)
-        .then(result => {
-          return result;
-        })
-        .catch(err => {
-          console.error("Erreur lors de l'insertion du document:", err);
-          throw err;
-        });
-    });
+  async insert(table, data) {
+    if (!this.connection) {
+      await this.initConnection();
+    }
+
+    try {
+      const result = await r.table(table).insert(data).run(this.connection);
+      return result;
+    } catch (err) {
+      console.error("Erreur lors de l'insertion du document:", err);
+      throw err;
+    }
   }
 
-  get(table, id) {
-    return this.connect().then(connection => {
-      return r
-        .table(table)
-        .get(id)
-        .run(connection)
-        .then(result => {
-          return result;
-        })
-        .catch(err => {
-          console.error('Erreur lors de la récupération du document:', err);
-          throw err;
-        });
-    });
+  async get(table, id) {
+    if (!this.connection) {
+      await this.initConnection();
+    }
+
+    return r.table(table)
+      .get(id)
+      .run(this.connection)
+      .then(result => result)
+      .catch(err => {
+        console.error('Erreur lors de la récupération du document:', err);
+        throw err;
+      });
   }
 
-  getAll(table) {
-    return this.connect().then(connection => {
-      return r
-        .table(table)
-        .run(connection)
-        .then(cursor => cursor.toArray())
-        .then(results => {
-          if (results.length > 0) {
-            return results; // Renvoie tous les documents correspondants
-          } else {
-            console.log('Aucun document trouvé.');
-            return [];
-          }
-        })
-        .catch(err => {
-          console.error('Erreur lors de la recherche des documents:', err);
-          throw err;
-        });
-    });
+  async getAll(table) {
+    if (!this.connection) {
+      await this.initConnection();
+    }
+
+    return r.table(table)
+      .run(this.connection)
+      .then(cursor => cursor.toArray())
+      .then(results => results.length > 0 ? results : [])
+      .catch(err => {
+        console.error('Erreur lors de la recherche des documents:', err);
+        throw err;
+      });
   }
 
   async getByWithFilter(table, filters) {
-    return this.connect().then(connection => {
-      let query = r.table(table);
+    if (!this.connection) {
+      await this.initConnection();
+    }
 
-      if (filters && Object.keys(filters).length > 0) {
-        query = query.filter(doc => {
-          return Object.keys(filters).map(key => {
-            if (key.includes('.')) {
-              // Gestion des champs imbriqués
-              const path = key.split('.');
-              let ref = doc;
-              path.slice(0, -1).forEach(p => {
-                ref = ref(p);
-              });
-
-              return ref(path[path.length - 1]).eq(filters[key]);
-            } else {
-              // Gestion des champs de premier niveau
-              return doc(key).eq(filters[key]);
-            }
-          }).reduce((left, right) => left.and(right));
-        });
-      }
-
-      return query
-        .run(connection)
-        .then(cursor => cursor.toArray())
-        .then(results => {
-          if (results.length > 0) {
-            return results;
+    let query = r.table(table);
+    if (filters && Object.keys(filters).length > 0) {
+      query = query.filter(doc => {
+        return Object.keys(filters).map(key => {
+          if (key.includes('.')) {
+            // Gestion des champs imbriqués
+            const path = key.split('.');
+            let ref = doc;
+            path.forEach(p => {
+              ref = ref(p);
+            });
+            return ref.eq(filters[key]);
           } else {
-            console.log('Aucun document trouvé avec les filtres fournis.');
-            return [];
+            // Gestion des champs de premier niveau
+            return doc(key).eq(filters[key]);
           }
-        })
-        .catch(err => {
-          console.error('Erreur lors de la recherche des documents:', err);
-          throw err;
-        });
-    });
+        }).reduce((left, right) => left.and(right));
+      });
+    }
+
+    return query.run(this.connection)
+      .then(cursor => cursor.toArray())
+      .then(results => results.length > 0 ? results : [])
+      .catch(err => {
+        console.error('Erreur lors de la recherche des documents:', err);
+        throw err;
+      });
   }
 
-  getFieldValues(table, field) {
-    return this.connect().then(connection => {
-      return r
-        .table(table)
-        .map(doc => doc(field))
-        .run(connection)
-        .then(cursor => cursor.toArray())
-        .catch(err => {
-          console.error(`Erreur lors de la récupération des valeurs du champ ${field}:`, err);
-          throw err;
-        });
-    });
+  async getFieldValues(table, field) {
+    if (!this.connection) {
+      await this.initConnection();
+    }
+
+    return r.table(table)
+      .map(doc => doc(field))
+      .run(this.connection)
+      .then(cursor => cursor.toArray())
+      .catch(err => {
+        console.error(`Erreur lors de la récupération des valeurs du champ ${field}:`, err);
+        throw err;
+      });
   }
 
-  update(table, data) {
-    return this.connect().then(connection => {
-      return r
-        .table(table)
-        .get(data.id)
-        .update(data, { nonAtomic: true })
-        .run(connection)
-        .then(result => {
-          console.log('document mis à jour avec succès');
-          return result;
-        })
-        .catch(err => {
-          console.error('Erreur lors de la mise à jour du document:', err);
-          throw err;
-        });
-    });
+  async update(table, data) {
+    if (!this.connection) {
+      await this.initConnection();
+    }
+
+    return r.table(table)
+      .get(data.id)
+      .update(data, { nonAtomic: true })
+      .run(this.connection)
+      .then(result => result)
+      .catch(err => {
+        console.error('Erreur lors de la mise à jour du document:', err);
+        throw err;
+      });
   }
 
-  remove(table, id) {
-    return this.connect().then(connection => {
-      return r
-        .table(table)
-        .get(id)
-        .delete()
-        .run(connection)
-        .then(result => {
-          console.log('document supprimé avec succès');
-          return result;
-        })
-        .catch(err => {
-          console.error('Erreur lors de la suppression du document:', err);
-          throw err;
-        });
-    });
+  async remove(table, id) {
+    if (!this.connection) {
+      await this.initConnection();
+    }
+
+    return r.table(table)
+      .get(id)
+      .delete()
+      .run(this.connection)
+      .then(result => result)
+      .catch(err => {
+        console.error('Erreur lors de la suppression du document:', err);
+        throw err;
+      });
   }
+
+  async createIndex(table, indexName) {
+    if (!this.connection) {
+      await this.initConnection();
+    }
+
+    return r.table(table)
+      .indexCreate(indexName)
+      .run(this.connection)
+      .then(result => result)
+      .catch(err => {
+        console.error(`Erreur lors de la création de l'index ${indexName}:`, err);
+        throw err;
+      });
+  }
+
+  async close() {
+    if (this.connection) {
+      try {
+        await this.connection.close();
+        this.connection = null;
+      } catch (err) {
+        console.error('Erreur lors de la fermeture de la connexion:', err);
+        throw err;
+      }
+    }
+  }
+
 }
+
 
 const db = new Database({ host: '127.0.0.1', port: 28015, db: 'orion' });
 
