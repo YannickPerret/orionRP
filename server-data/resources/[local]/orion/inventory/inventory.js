1a434e952388179b3715cecbe5dc3fd2b78108c4
Yannick Perret <contact@yannickperret.com>
2023-12-28 21:44:59 +0100

    update inventory class

--- server-data/resources/[local]/orion/inventory/inventory.js
+++ server-data/resources/[local]/orion/inventory/inventory.js
@@ -50,7 +50,7 @@ class Inventory {
 
     async getItem(itemId) {
         const item = await Item.getById(itemId);
-        item.quantity = this.items.find(i => i.id === itemId).quantity;
+        item.quantity = this.items.find(i => i.itemId === itemId).quantity;
         return item
     }
 
