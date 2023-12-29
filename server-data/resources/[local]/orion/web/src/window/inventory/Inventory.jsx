6749579d6296e01b93bef23779923a056a006d67
Yannick Perret <contact@yannickperret.com>
2023-12-29 14:18:33 +0100

    update inventory class

--- server-data/resources/[local]/orion/web/src/window/inventory/Inventory.jsx
+++ server-data/resources/[local]/orion/web/src/window/inventory/Inventory.jsx
@@ -43,7 +43,6 @@ export default function Inventory() {
     const handleUseItem = (id, shouldClose) => {
         sendToNui('useItem', { id })
         if (shouldClose) {
-            console.log('should close');
             closeAllMenus(true);
         }
     }
