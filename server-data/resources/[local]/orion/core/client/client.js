0233b95b71863dade2bfc3194e97c775c5d54c71
Yannick Perret <contact@yannickperret.com>
2024-01-04 10:08:55 +0100

    garage

--- server-data/resources/[local]/orion/core/client/client.js
+++ server-data/resources/[local]/orion/core/client/client.js
@@ -109,9 +109,9 @@ onNet('orion:blips:c:createBlips', async (blips) => {
     exports['orion'].createBlip(bank.position, 108, 2, bank.name);
   });
 
-  blips.garages.forEach(garage => {
+  /*blips.garages.forEach(garage => {
     exports['orion'].createBlip(garage.position, 357, 4, garage.name);
-  });
+  });*/
 });
 
 onNet('orion:core:c:animations:playAnimation', async (dict, anim, duration, flag, flag2, flag3, flag4, flag5) => {
