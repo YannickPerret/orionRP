b0146e14d3a5b21980283901830dbafd74687aea
Yannick Perret <contact@yannickperret.com>
2024-01-04 18:58:40 +0100

    garage

--- server-data/resources/[local]/orion/garage/client.js
+++ server-data/resources/[local]/orion/garage/client.js
@@ -19,8 +19,7 @@
             let playerCoords = GetEntityCoords(playerPed);
             garageJson.garages.forEach(garage => {
                 if (GetDistanceBetweenCoords(playerCoords[0], playerCoords[1], playerCoords[2], garage.dispawnPlace.X, garage.dispawnPlace.Y, garage.dispawnPlace.Z, true) < 15) {
-                    DrawMarker(1, garage.x, garage.y, garage.z + 2, 0.0, 0.0, 0.0, 0.0, 180.0, 0, 2.0, 2.0, 2.0, 255, 128, 0, 50, false, true, 2, false, false, false, false)
-                    console.log('test')
+                    DrawMarker(1, garage.dispawnPlace.X, garage.dispawnPlace.Y, garage.dispawnPlace.Z + 2, 0.0, 0.0, 0.0, 0.0, 180.0, 0, 2.0, 2.0, 2.0, 255, 128, 0, 50, false, true, 2, false, false, false, false)
                     if (GetDistanceBetweenCoords(playerCoords[0], playerCoords[1], playerCoords[2], garage.dispawnPlace.X, garage.dispawnPlace.Y, garage.dispawnPlace.Z, true) < 2) {
                         DrawText3D(garage.dispawnPlace.X, garage.dispawnPlace.Y, garage.dispawnPlace.Z, '~g~E~w~ - Rentrer la voiture dans le garage')
                         if (IsControlJustReleased(0, 38)) {
