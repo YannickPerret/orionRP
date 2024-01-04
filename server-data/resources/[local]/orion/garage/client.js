7c435c20f03d32e13757d5460d20126b0660b5f2
Yannick Perret <contact@yannickperret.com>
2024-01-04 18:24:46 +0100

    garage

--- server-data/resources/[local]/orion/garage/client.js
+++ server-data/resources/[local]/orion/garage/client.js
@@ -4,7 +4,7 @@
 // spawn la voiture à un emplacement libre du parking, sinon interdir le spawn si pas libre
 // créer un marker pour ranger la voiture de chaque parking
 (async () => {
-    const garageJson = JSON.parse(LoadResourceFile(GetCurrentResourceName(), 'garage/garage.json')).garages
+    const garageJson = JSON.parse(LoadResourceFile(GetCurrentResourceName(), 'garage/garage.json'))
 
     const parking = []
 
@@ -17,7 +17,7 @@
         while (true) {
             let playerPed = PlayerPedId();
             let playerCoords = GetEntityCoords(playerPed);
-            garageJson.forEach(garage => {
+            garageJson.garages.forEach(garage => {
                 if (GetDistanceBetweenCoords(playerCoords[0], playerCoords[1], playerCoords[2], garage.x, garage.y, garage.z, true) < 5) {
                     DrawMarker(1, garage.x, garage.y, garage.z - 1, 0, 0, 0, 0, 0, 0, 1.0, 1.0, 0.5, 0, 255, 0, 100, false, true, 2, false, false, false, false)
                     if (IsControlJustReleased(0, 38)) {
