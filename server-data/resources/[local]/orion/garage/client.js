3ec15af6b1454a5ab3ab78fc9efde25a0a49147f
Yannick Perret <contact@yannickperret.com>
2024-01-04 20:06:43 +0100

    garage

--- server-data/resources/[local]/orion/garage/client.js
+++ server-data/resources/[local]/orion/garage/client.js
@@ -58,11 +58,13 @@
             garageJson.garages.forEach(garage => {
                 if (GetDistanceBetweenCoords(playerCoords[0], playerCoords[1], playerCoords[2], garage.dispawnPlace.X, garage.dispawnPlace.Y, garage.dispawnPlace.Z, true) < 15) {
                     DrawMarker(1, garage.dispawnPlace.X, garage.dispawnPlace.Y, garage.dispawnPlace.Z, 0.0, 0.0, 0.0, 0.0, 180.0, 0, 3.0, 3.0, 2.0, 0, 128, 0, 50, false, true, 2, false, false, false, false)
-                    if (GetDistanceBetweenCoords(playerCoords[0], playerCoords[1], playerCoords[2], garage.dispawnPlace.X, garage.dispawnPlace.Y, garage.dispawnPlace.Z, true) < 1.8) {
-                        //exports['orion'].draw3DText(garage.dispawnPlace.X, garage.dispawnPlace.Y, garage.dispawnPlace.Z + 1, `Appuyez sur ~g~E~w~ pour rentrer la voiture dans le garage`)
-                        emit('orion:showText', `Appuyez sur ~g~E~w~ pour ouvrir le garage`)
-                        if (IsControlJustReleased(0, 38)) {
-                            emitNet('orion:garage:s:openGarage', garage.id)
+                    if (!showGarageHUD) {
+                        if (GetDistanceBetweenCoords(playerCoords[0], playerCoords[1], playerCoords[2], garage.dispawnPlace.X, garage.dispawnPlace.Y, garage.dispawnPlace.Z, true) < 1.8) {
+                            //exports['orion'].draw3DText(garage.dispawnPlace.X, garage.dispawnPlace.Y, garage.dispawnPlace.Z + 1, `Appuyez sur ~g~E~w~ pour rentrer la voiture dans le garage`)
+                            emit('orion:showText', `Appuyez sur ~g~E~w~ pour ouvrir le garage`)
+                            if (IsControlJustReleased(0, 38)) {
+                                emitNet('orion:garage:s:openGarage', garage.id)
+                            }
                         }
                     }
                 }
