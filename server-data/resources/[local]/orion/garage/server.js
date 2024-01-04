71cdd7a1879b20b41b4313648fc088e6f1ab01b3
Yannick Perret <contact@yannickperret.com>
2024-01-04 16:05:45 +0100

    garage

--- server-data/resources/[local]/orion/garage/server.js
+++ server-data/resources/[local]/orion/garage/server.js
@@ -1,12 +1,5 @@
 (async () => {
-    const GarageManager = require('./core/server/garageManager.js');
-    const PlayerManager = require('./core/server/playerManager.js');
 
-    onNet('orion:garage:s:initGarages', async (garages) => {
-        GarageManager.getAll().then((garages) => {
-            GarageManager.addGarage(garages.id, garages);
-        })
-        console.log("garages")
-    })
+
 
 })();
