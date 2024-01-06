3be1dc408df812800706ad9a8bc1862739f995fe
Yannick Perret <contact@yannickperret.com>
2024-01-06 01:41:56 +0100

    update page

--- server-data/resources/[local]/orion/core/client/marker.js
+++ server-data/resources/[local]/orion/core/client/marker.js
@@ -13,8 +13,7 @@
                 let playerPed = PlayerPedId();
                 let playerCoords = GetEntityCoords(playerPed);
                 markers.forEach(marker => {
-                    console.log(GetDistanceBetweenCoords(playerCoords[0], playerCoords[1], playerCoords[2], marker.position.X, marker.position.Y, marker.position.Z, true))
-                    if (GetDistanceBetweenCoords(playerCoords[0], playerCoords[1], playerCoords[2], marker.position.X, marker.position.Y, marker.position.Z, true) < 15) {
+                    if (GetDistanceBetweenCoords(playerCoords[0], playerCoords[1], playerCoords[2], marker.position.X, marker.position.Y, marker.position.Z, true) < 25) {
                         console.log('draw marker')
                         createMarker(marker.position, marker.color, marker.icon, marker.scale);
                     }
