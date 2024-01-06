16dfdad61103a0823fe24d68788992e124e10223
Yannick Perret <contact@yannickperret.com>
2024-01-06 17:35:15 +0100

    update page

--- server-data/resources/[local]/orion/garage/client.js
+++ server-data/resources/[local]/orion/garage/client.js
@@ -33,6 +33,7 @@
         const vehicleModelName = GetDisplayNameFromVehicleModel(GetEntityModel(vehicleTarget));
         const vehicleDamage = await exports['orion'].getVehicleDamage(vehicleTarget);
 
+        console.log(vehicleModelName)
         emitNet('orion:garage:s:storeVehicle', NetworkGetNetworkIdFromEntity(vehicleTarget), vehicleModelName, vehicleDamage, data.garageId);
         cb({ ok: true });
     });
