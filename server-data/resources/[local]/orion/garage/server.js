09e04143bba402a008e85ab55482c54555073f62
Yannick Perret <contact@yannickperret.com>
2024-01-07 02:18:44 +0100

    update page

--- server-data/resources/[local]/orion/garage/server.js
+++ server-data/resources/[local]/orion/garage/server.js
@@ -18,7 +18,6 @@
         const player = PlayerManager.getPlayerBySource(source);
         const garage = GarageManager.getGarageByMarkerPosition(garageMarker)
 
-        console.log("test")
         const vehicles = await garage.getVehicles();
         vehicles.forEach(vehicle => {
             // Calcul du temps en heures
