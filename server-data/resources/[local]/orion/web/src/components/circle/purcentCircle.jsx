b874d30abc778b7151227e2c6eb3a1e9529cff02
Yannick Perret <contact@yannickperret.com>
2024-01-07 23:54:39 +0100

    update gameversion

--- server-data/resources/[local]/orion/web/src/components/circle/purcentCircle.jsx
+++ server-data/resources/[local]/orion/web/src/components/circle/purcentCircle.jsx
@@ -4,7 +4,10 @@ import style from './purcentBar.module.scss';
 export default function CircleBar({ percentage, color, image }) {
     return (
         <div className={style.circle__container}>
+            sfdsfsdfs
             <div className={style.circle__bar} style={{ width: `${percentage}%`, background: `${color}` }}></div>
+            {//<img src={image} alt="Icône" class="circle__icon" />
+            }
         </div>
     );
 }
