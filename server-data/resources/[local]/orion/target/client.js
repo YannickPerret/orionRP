09aebe2c3b523ec97c6ed1eaef18adc9624f1a07
Yannick Perret <contact@yannickperret.com>
2024-01-11 12:30:01 +0100

    targeting

--- server-data/resources/[local]/orion/target/client.js
+++ server-data/resources/[local]/orion/target/client.js
@@ -52,7 +52,6 @@
             y: cameraCoord.y + direction.y * distance,
             z: cameraCoord.z + direction.z * distance
         }
-        console.log("gvgfSS")
         let [a, hit, coords, d, entity] = GetShapeTestResult(StartShapeTestRay(cameraCoord.x, cameraCoord.y, cameraCoord.z, destination.x, destination.y, destination.z, -1, PlayerPedId(), 0));
         if (exports['orion'].getDistanceBetweenCoords(cameraCoord, coords) < distance) {
             return [hit, coords, entity];
