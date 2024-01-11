b9607cf2b2f431c8be8c8d6d7bfb1526c5a5b5e7
Yannick Perret <contact@yannickperret.com>
2024-01-11 17:14:53 +0100

    targeting

--- server-data/resources/[local]/orion/target/client.js
+++ server-data/resources/[local]/orion/target/client.js
@@ -104,7 +104,7 @@
                 }
             }
             else {
-                let [haveHit, entityCoords, entityHit] = rayCastGamePlayCamera(10);
+                let [haveHit, entityCoords, entityHit] = rayCastGamePlayCamera(2);
                 //get type of entity and set targetValue by type
                 console.log(haveHit)
                 if (haveHit) {
