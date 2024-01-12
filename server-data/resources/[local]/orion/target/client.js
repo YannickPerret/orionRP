d525a709b9955c8fdf8b4c732735dfb8ec670c2b
Yannick Perret <contact@yannickperret.com>
2024-01-12 12:19:28 +0100

    target

--- server-data/resources/[local]/orion/target/client.js
+++ server-data/resources/[local]/orion/target/client.js
@@ -130,7 +130,7 @@
             /*SetDrawOrigin(GetEntityCoords(playerPed), 0);
             DrawSprite(dict, texture, 0, 0, 0.02, 0.035, 0, 255, 255, 255, 255);
             ClearDrawOrigin();*/
-            DrawSprite(dict, texture, 0.5, 0.5, 0.03, 0.03, 0, 255, 255, 255, 255)
+            DrawSprite(dict, texture, 0.5, 0.5, 0.01, 0.01, 0, 255, 255, 255, 255)
 
             console.log("is active")
 
