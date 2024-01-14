155762de77b2f5127380ce3f3010588d7db42848
Yannick Perret <contact@yannickperret.com>
2024-01-14 22:46:31 +0100

    update readme

--- server-data/resources/[local]/orion/core/server/migrations/0018_insert_table_job.js
+++ server-data/resources/[local]/orion/core/server/migrations/0018_insert_table_job.js
@@ -3,7 +3,7 @@ module.exports = {
     version: 18,
     migrate: async (db) => {
         // your code here
-        console.log(jobs)
+        console.log(jobs.jobs)
         jobs.jobs.forEach(async (job, index) => {
             await db.insert('jobs', {
                 name: job.name,
