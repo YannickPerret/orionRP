78ceff151f1fe3179fb013dedb0c08fafcbf8692
tchoune <contact@yannickperret.com>
2024-10-30 02:54:16 +0100

    import db

--- server-data/resources/orionCore/server/main.js
+++ server-data/resources/orionCore/server/main.js
@@ -16,7 +16,7 @@
             console.log('Connecté à la base de données MySQL avec TypeORM');
 
             // Initialiser les items
-            const initItems = require('./scripts/initItems.js');
+            const initItems = require('./server/scripts/initItems.js');
             await initItems();
 
             // Importer les événements serveur
