627f2154159f87f5889c736549b41c8a0a0f43e2
tchoune <contact@yannickperret.com>
2024-11-04 10:01:45 +0100

    chore : add image loading

--- server-data/resources/[core]/loadingScreen/web/src/App.js
+++ server-data/resources/[core]/loadingScreen/web/src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className={"flex w-full h-screen"}
-           <img src={
+      <div className={"flex w-full h-screen"}>
+           <img src={'./images/screenload1.jpg'} alt={'loading'} className={'object-cover w-full h-full'} />
       </div>
     </div>
   );
